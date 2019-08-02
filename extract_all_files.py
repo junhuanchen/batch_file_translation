6cf1770f4dcabc2ab5c075f2549a4207aa9423d3
Juwan <junhuanchen@qq.com>
2019-08-02 14:11:14 +0800

    add more extract dir

--- extract_all_files.py
+++ extract_all_files.py
@@ -25,13 +25,36 @@ def all_mkdir(path):
     return os.path.exists(path)
 
 
-def extract_file(source_dir='test', goal_dir='result\\', file_set=['.txt', '.rst', '.md'], function=lambda t:t):
+def extract_file_by_suffix(source_dir='test', goal_dir='result\\', file_set=['.txt', '.rst', '.md'], function=lambda t:t):
     files = tree_files(source_dir)
     # print(files)
     for fe in files:
-        print(fe)
+        # print(fe, get_file_type(fe))
         if get_file_type(fe) in file_set:
+            print(fe)
+            
+            res = ''
 
+            with open(fe, 'r', encoding='utf-8') as f:
+                # print(f.read())
+                res = f.read()
+
+            path = os.path.dirname(fe)
+            all_mkdir(goal_dir + path)
+
+            res = function(res)
+
+            with open(goal_dir + fe, 'w', encoding='utf-8') as f:
+                (f.write(res))
+
+def extract_file_by_name(source_dir='test', goal_dir='result\\', file_set=['a.txt', 'c.rst', 'b.md'], function=lambda t:t):
+    files = tree_files(source_dir)
+    # print(files)
+    for fe in files:
+        # print(fe, os.path.basename(path))
+        if os.path.basename(fe) in file_set:
+            print(fe)
+            
             res = ''
 
             with open(fe, 'r', encoding='utf-8') as f:
@@ -50,5 +73,9 @@ def test(t):
     return 'test\n' + t 
 
 if __name__ == "__main__":
-    import google_translate
-    extract_file(source_dir='test', goal_dir='result\\', function=google_translate.get_translate_zh)
+    # import google_translate
+    # extract_file(source_dir='test', goal_dir='result\\', function=google_translate.get_translate_zh)
+
+    extract_file_by_name(source_dir='test', goal_dir='name\\')
+
+    extract_file_by_suffix(source_dir='test', goal_dir='suffix\\')
